ed2a91427b64d641ae5c9173e15cdfd510414d4d
Cal Crosby <calcruiseby@gmail.com>
2024-11-03 20:45:31 -0600

    added revision in the source

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from 'astro/config';
+import { execSync } from 'child_process';
 
-// https://astro.build/config
+// Fetch the latest Git commit hash
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+
 export default defineConfig({
-    site: 'https://dacrosbycode.com'
+  site: 'https://dacrosbycode.com',
+  vite: {
+    plugins: [
+      {
+        name: 'vite-plugin-revision',
+        config: () => ({
+          define: {
+            'import.meta.env.REVISION': JSON.stringify(commitHash),
+          },
+        }),
+      },
+    ],
+  },
 });
-
